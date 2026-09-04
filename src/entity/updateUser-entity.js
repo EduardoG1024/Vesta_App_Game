9338a6e8b259587799c5d1ecb03685b21f88307f
EduardoG1024 <spawnlol2424@gmail.com>
2026-09-04 15:30:08 -0700

    UPDATE updateEntity

--- src/entity/updateUser-entity.js
+++ src/entity/updateUser-entity.js
@@ -1,12 +1,12 @@
 
 export class UpdateEntity {
 
-    constructor(usertag, main, role, level, rank, others) {
+    constructor(usertag, main, role, level, rank, platform) {
         this.usertag = usertag;
         this.mainCharacter = main;
         this.role = role;
         this.level = level;
         this.rank = rank;
-        this.others = others;
+        this.platform = platform;
     }
 }
